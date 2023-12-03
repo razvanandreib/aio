ef0f5da646fad8e97b2ecfcf13bc258b39cafea6
razvanandreib <razvanandreibalauru@gmail.com>
2023-12-03 13:04:50 +0200

    placeholder

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+    image: {
+        service: {
+            entrypoint: "astro/assets/services/noop"
+        }
+    }
+})
