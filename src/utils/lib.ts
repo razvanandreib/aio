38441f12378cbf67c3b357d50580c3d6857f47f1
razvanandreib <razvanandreibalauru@gmail.com>
2023-12-03 19:28:26 +0200

    done first page

---
+++ src/utils/lib.ts
@@ -0,0 +1,29 @@
+export const navLinks = [
+  {
+    link: "/misiune",
+    text: "Misiunea Noastra",
+  },
+  {
+    link: "/implicate",
+    text: "Implicate",
+  },
+  {
+    link: "/noutati-stiri",
+    text: "Noutati si Stiri",
+  },
+];
+
+export const socialLinks = [
+  {
+    link: "#",
+    text: "Facebook",
+  },
+  {
+    link: "#",
+    text: "Instagram",
+  },
+  {
+    link: "#",
+    text: "Youtube",
+  },
+];
